0093a3b78fbaa216bb16adce267533df7d359f87
Pavan699 <pavannakate03@gmail.com>
2021-09-07 10:07:50 -0700

    Repalce If-Else with Switch-Case

--- Refactor_Emp_Wage/Program.cs
+++ Refactor_Emp_Wage/Program.cs
@@ -7,19 +7,25 @@ namespace Refactor_Emp_Wage
         public void check()
         {
             Random emp_random = new Random();
-            int empcheck = emp_random.Next(0, 2);
+            int empcheck = emp_random.Next(0, 3);
             int oneDay_Sal = 0, full_Time = 8, perHour_Pay = 20, part_Time = 4;
-            if (empcheck == 1)
+            switch (empcheck)
             {
-                Console.WriteLine("It's Work-Type is : Full-Time");
-                oneDay_Sal = full_Time * perHour_Pay;              
+                case 1:
+                    Console.WriteLine("It's Work-Type is : Full-Time");
+                    oneDay_Sal = full_Time * perHour_Pay;
+                    Console.WriteLine("One Day Salary is : {0}", oneDay_Sal);
+                    break;
+                case 2:
+                    Console.WriteLine("It's Work-Type is : Part-Time");
+                    oneDay_Sal = part_Time * perHour_Pay;
+                    Console.WriteLine("One Day Salary is : {0}", oneDay_Sal);
+                    break;
+                default:
+                    Console.WriteLine(" Wrong Input :)");
+                    break;
             }
-            else
-            {
-                Console.WriteLine("It's Work-Type is : Part-Time");
-                oneDay_Sal = part_Time * perHour_Pay;              
-            }
-            Console.WriteLine("One Day Salary is : {0}", oneDay_Sal);
+
         }
     }
     class Program
