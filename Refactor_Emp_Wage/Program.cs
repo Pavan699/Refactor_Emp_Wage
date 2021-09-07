ef0ea36c6a8a77ca94c837fb2e3a51f790393a40
Pavan699 <pavannakate03@gmail.com>
2021-09-07 09:52:02 -0700

    Adding Full-Time Wage

--- Refactor_Emp_Wage/Program.cs
+++ Refactor_Emp_Wage/Program.cs
@@ -8,9 +8,12 @@ namespace Refactor_Emp_Wage
         {
             Random emp_random = new Random();
             int empcheck = emp_random.Next(0, 2);
+            int oneDay_Sal = 0, full_Time = 8, perHour_Pay = 20;
             if (empcheck == 1)
             {
-                Console.WriteLine("Employee is Present ");
+                Console.WriteLine("It's Work-Type is : Full-Time");
+                oneDay_Sal = full_Time * perHour_Pay;
+                Console.WriteLine("One Day Salary is : {0}", oneDay_Sal);
             }
             else
             {
