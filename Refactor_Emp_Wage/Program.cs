5b0ca95f28fe03bec22c42b4f015b625ed3ea61a
Pavan699 <pavannakate03@gmail.com>
2021-09-07 10:48:46 -0700

    Logic for Checking Hours and Days

--- Refactor_Emp_Wage/Program.cs
+++ Refactor_Emp_Wage/Program.cs
@@ -8,26 +8,41 @@ namespace Refactor_Emp_Wage
         {
             int total_Sal = 0;
             total_Sal = oneDay_Sal * days;
-            Console.WriteLine("Total Work-Days Salary is : {0}", total_Sal);
+            Console.WriteLine("Total Work-Days Salary is : {0}", total_Sal);         
+        }
+        public void hours(int work_Hours, int days)
+        {
+            if (work_Hours > 100 || days >= 20)
+            {
+                Console.WriteLine("Working hours are {0} And Work Days are {1}", work_Hours, days);
+            }
+            else
+            {
+                Console.WriteLine("Working Hours Are less than 100 ");
+            }
         }
         public void check()
         {
             Random emp_random = new Random();
             int empcheck = emp_random.Next(0, 3);
-            int oneDay_Sal = 0, full_Time = 8, perHour_Pay = 20, part_Time = 4, Work_days = 20;
+            int oneDay_Sal = 0, full_Time = 8, perHour_Pay = 20, part_Time = 4, Work_days = 20, total_WorkHour = 0;
             switch (empcheck)
             {
                 case 1:
                     Console.WriteLine("It's Work-Type is : Full-Time");
                     oneDay_Sal = full_Time * perHour_Pay;
                     Console.WriteLine("One Day Salary is : {0}", oneDay_Sal);
+                    total_WorkHour = full_Time * Work_days;
                     monthlyPay(oneDay_Sal, Work_days);
+                    hours(total_WorkHour, Work_days);
                     break;
                 case 2:
                     Console.WriteLine("It's Work-Type is : Part-Time");
                     oneDay_Sal = part_Time * perHour_Pay;                   
                     Console.WriteLine("One Day Salary is : {0}", oneDay_Sal);
+                    total_WorkHour = part_Time * Work_days;
                     monthlyPay(oneDay_Sal, Work_days);
+                    hours(total_WorkHour, Work_days);
                     break;
                 default:
                     Console.WriteLine(" Wrong Input :)");
