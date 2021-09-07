aef392aa5d298f0f379aac5fdf443757e1b358ff
Pavan699 <pavannakate03@gmail.com>
2021-09-07 09:17:04 -0700

    Adding Title

---
+++ Refactor_Emp_Wage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Refactor_Emp_Wage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wages");
+        }
+    }
+}
