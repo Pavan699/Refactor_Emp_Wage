5fc787be432c883d2aac5c94ecddafcd6fe13086
Pavan699 <pavannakate03@gmail.com>
2021-09-07 09:45:54 -0700

    Random For Present OR Absent

--- Refactor_Emp_Wage/Program.cs
+++ Refactor_Emp_Wage/Program.cs
@@ -2,11 +2,30 @@
 
 namespace Refactor_Emp_Wage
 {
-    class Program
+    class Calculate
     {
+        public void check()
+        {
+            Random emp_random = new Random();
+            int empcheck = emp_random.Next(0, 2);
+            if (empcheck == 1)
+            {
+                Console.WriteLine("Employee is Present ");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent ");
+            }
+        }
+    }
+    class Program
+    {       
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wages");
+            Calculate cal = new Calculate();
+            cal.check();
+
         }
     }
 }
