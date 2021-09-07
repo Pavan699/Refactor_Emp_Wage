19f9b801948284b4f8b6854df6984c3db88bba1b
Pavan699 <pavannakate03@gmail.com>
2021-09-07 10:00:33 -0700

    Adding Part-Time Wage

--- Refactor_Emp_Wage/Program.cs
+++ Refactor_Emp_Wage/Program.cs
@@ -8,17 +8,18 @@ namespace Refactor_Emp_Wage
         {
             Random emp_random = new Random();
             int empcheck = emp_random.Next(0, 2);
-            int oneDay_Sal = 0, full_Time = 8, perHour_Pay = 20;
+            int oneDay_Sal = 0, full_Time = 8, perHour_Pay = 20, part_Time = 4;
             if (empcheck == 1)
             {
                 Console.WriteLine("It's Work-Type is : Full-Time");
-                oneDay_Sal = full_Time * perHour_Pay;
-                Console.WriteLine("One Day Salary is : {0}", oneDay_Sal);
+                oneDay_Sal = full_Time * perHour_Pay;              
             }
             else
             {
-                Console.WriteLine("Employee is Absent ");
+                Console.WriteLine("It's Work-Type is : Part-Time");
+                oneDay_Sal = part_Time * perHour_Pay;              
             }
+            Console.WriteLine("One Day Salary is : {0}", oneDay_Sal);
         }
     }
     class Program
